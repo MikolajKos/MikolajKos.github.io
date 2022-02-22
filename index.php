b9f32e99008205e4488df7b5eb2a4d9e6820e72d
MikolajKos <niko.kosiorek@gmail.com>
2022-02-22 18:23:39 +0100

    change

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta charset="UTF-8">
         <title>ModernWEB</title>
         <link rel="stylesheet" href="style.css">
-
+        <meta http-equiv="X-UA-Compatible">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
